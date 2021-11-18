e61090465e1d68728ad32379b87c63cd56c45a9a
Ryan Owen Thionanda <owenizedd@gmail.com>
2021-11-18 10:09:48 +0700

    better ui

--- src/components/EmojiPicker.js
+++ src/components/EmojiPicker.js
@@ -11,10 +11,10 @@ export default function EmojiPicker({onPick, isShowing, toggleShowing}){
             <p className="emoji-picker--button cursor-pointer bg-gray-800 rounded-lg pl-2 text-xl" onClick={toggleShowing}>+</p>
             {   
                 isShowing &&
-                <div className="emoji-picker--list rounded-lg bg-gray-900 px-3 py-1 pt-12">
+                <div className="emoji-picker--list rounded-lg bg-gray-900 bg-opacity-75 px-3 py-1 pt-12">
                     <div className='absolute flex w-11/12 justify-between pr-1 left-0 top-0 text-lg ml-5 mt-2'>
-                        <span>Pick an emoji to react</span>
-                        <span className="text-3xl -top-2 relative" onClick={toggleShowing}>x</span>
+                        <span className="font-bold">Pick an emoji to react</span>
+                        <span className="text-3xl -top-2 relative rounded-lg bg-gray-900 py-1 px-5" onClick={toggleShowing}>x</span>
                     </div>
                     { emojiUnicodes.map(emoji => <span className='lg:text-lg cursor-pointer mx-1' onClick={() => handleEmojiPick(emoji)}>{emoji}</span>)}
                 </div>
